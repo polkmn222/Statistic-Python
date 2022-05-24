62fad2259fd15fb07766b2e0704fe84ab02d187c
SangYeol-github <polkmn222@naver.com>
2022-05-24 17:59:28 +0900

    정규수업

---
+++ 0524/1-2)문자형.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. 문자열 생성방법
+
+# In[2]:
+
+
+# 첫번째 방법
+
+"Life is too short, you need python"
+
+
+# In[4]:
+
+
+# 두번째 방법
+'Life is too short, you need python'
+
+
+# In[5]:
+
+
+# 세번째 방법
+"""Life is too short, you need python"""
+
+
+# In[6]:
+
+
+# 네번째 방법
+'''Life is too short, you need python'''
+
+
+# In[7]:
+
+
+# 첫번째 방법의 오류로
+# 두번째 방법을 개발
+"python`s favorite food is perl"
+
+
+# In[9]:
+
+
+# 두번째 방법의 오류로
+# 첫번째 방법을 개발
+print('"I like him" set says.')
+
+
+# In[10]:
+
+
+# 포메팅 방법
+"현재 온도는 30도입니다."
+"현재 온도는 17도입니다."
+
+
+# In[11]:
+
+
+"현재 온도는 %d도입니다" % 30
+
